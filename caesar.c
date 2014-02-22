16eff7f25a7d1baa0e8bd634a6dec816af15dc36
Emily Veach <veachemily@gmail.com>
2014-02-21 19:19:58 -0500

    fixed formatting

--- caesar.c
+++ caesar.c
@@ -14,49 +14,49 @@
 
 int main(int argc, string argv[])
 {
-//Verify the command-line argument exists
     string phrase = "";
     int k;
-    
+
+    //Verify the command-line argument exists
     if (argc != 2)
-        {
-            printf("Try again with a positive number\n");
-            return 1;
-        }
+    {
+        printf("Try again with a positive number\n");
+        return 1;
+    }
     else
-        {
-            //Convert the cypher to an integer from a string 
-            k = atoi(argv[1]);
-            //Set the cypher to be 1-26
-            k = k % 26;
-            
-            //Ask the user for a string to be encrypted
-            //printf("Please enter a phrase to be encoded: \n");
-            phrase = GetString();
-        }
+    {
+        //Convert the cypher to an integer from a string 
+        k = atoi(argv[1]);
+        //Set the cypher to be 1-26
+        k = k % 26;
+        
+        //Ask the user for a string to be encrypted
+        //printf("Please enter a phrase to be encoded: \n");
+        phrase = GetString();
+    }
         
     //Shift the characters using the command-line input
     for (int i = 0, n = strlen(phrase); i <= n - 1; i++)
-        {            
-            if (isalpha(phrase[i]))
+    {            
+        if (isalpha(phrase[i]))
+        {
+            char b = phrase[i]; 
+            if ((isupper(b) && ((b + k) > 90)) || (islower(b) && ((b + k) > 122)))
             {
-                char b = phrase[i]; 
-                if ((isupper(b) && ((b + k) > 90)) || (islower(b) && ((b + k) > 122)))
-                {
-                    b = b - (26 - k);
-                    printf("%c", b);
-                }
-                
-                else 
-                {
-                    b = b + k;
-                    printf("%c", b);
-                }
+                b = b - (26 - k);
+                printf("%c", b);
             }
+            
             else 
             {
-                printf("%c", phrase[i]);
+                b = b + k;
+                printf("%c", b);
             }
         }
-      printf("\n");  
+        else 
+        {
+            printf("%c", phrase[i]);
+        }
+    }
+    printf("\n");  
 }
