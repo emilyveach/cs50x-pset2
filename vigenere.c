c3e2e006f7cb562da4ea58f2d1033d0475e6053f
Emily Veach <veachemily@gmail.com>
2014-04-10 16:43:33 -0400

    added a letter shifter function and used it in a new vigenere cipher function...

--- vigenere.c
+++ vigenere.c
@@ -12,14 +12,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-string caesar_cipher(int cipher, string phrase);
+char letter_shifter(char ch, int shift);
 string vigenere_cipher(string phrase, string keyword);
+
 
 int main(int argc, string argv[])
 {
     string keyword = argv[1];
     
-    //verify the command line argument is all alphabetical; if not, return 1 and exit
+    //program uses main() to verify keyword as all alpha
     if (argc != 2)
     {
         printf("Please enter a command-line argument.\n");
@@ -27,70 +28,64 @@ int main(int argc, string argv[])
     }
     for (int i = 0, n = strlen(keyword); i < n; i++)
     {
-        if ((isalpha(keyword[i])) == 0)
+        if (!isalpha(keyword[i]))
         {
             printf("Please enter an all-alphabetical keyword.\n");
             return 1;
         }
     }
-    
+    //program uses main() to prompt and save phrase from user
     string phrase = GetString();
     
+    //main prints the result of vigenere_cipher()
     printf("%s\n", vigenere_cipher(phrase, keyword));
 }
-
 
 
 string vigenere_cipher(string phrase, string keyword)
 {
     int keywordlength = strlen(keyword);
+    char keychar;
     
+    //vigenere_cipher() progresses through the phrase and keyword at each iteration
     for (int i = 0, k = 0, n = strlen(phrase); i < n; i++, k++)
     {
-        char phraseAt = phrase[i];
-        char keywordAt = keyword[k % keywordlength];
-        int cipher = keywordAt;
+        keychar = keyword[k % keywordlength];
         
-        if (isalpha(phraseAt))
+        //program uses vigenere_cipher() to normalize 
+        // the k-th letter of the keyword for use by letter_shifter()
+        if (isupper(keychar))
         {
-            return caesar_cipher(cipher, phrase);
-            k++;
+            keychar -= 'A';
         }
         else 
         {
-            phraseAt = phrase[i];
-            k--;
-            continue;
+            keychar -= 'a';
         }
+        
+        //program uses vigenere_cipher() to send the i-th letter of the phrase with 
+        //the corresponding cipher letter to letter_shifter()
+        phrase[i] = letter_shifter(phrase[i], keychar);
+        if (!isalpha(phrase[i]))
+        {
+            k--;
+        }
+        
     }
     
     return phrase;
 }
 
-string caesar_cipher(int cipher, string phrase)
-{
-    char phraseAt;
-    
-    //Shift the characters using the command-line input
-    for (int i = 0, n = strlen(phrase); i < n; i++)
+char letter_shifter(char ch, int shift)
+{   
+    if (isupper(ch))
     {
-        phraseAt = phrase[i];
-        
-        if (!isalpha(phraseAt))
-        {
-            phrase[i] = phraseAt;
-            continue;
-        }
-            
-        if (isupper(phraseAt))
-        {
-            phrase[i] = (phraseAt - 'A' + cipher) % 26 + 'A';
-        }
-        else
-        {
-            phrase[i] = (phraseAt - 'a' + cipher) % 26 + 'a';
-        }
+        ch = (ch - 'A' + shift) % 26 + 'A';
+    }
+    else if (islower(ch))
+    {
+        ch = (ch - 'a' + shift) % 26 + 'a';
     }
     
-    return phrase;
+    return ch;
 }
